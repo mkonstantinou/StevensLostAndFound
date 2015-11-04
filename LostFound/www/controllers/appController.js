2564d27b5263953e76cea58e43032c21ec38f426
mkonstantinou <matt.konstantinou@gmail.com>
2015-11-03 23:39:32 -0500

    add controller for application template

---
+++ LostFound/www/controllers/appController.js
@@ -0,0 +1,29 @@
+angular.module( 'starter' )
+	.controller( 'appController', function ( $scope, $ionicSideMenuDelegate ) 
+	{
+		$scope.items = [
+			{
+				title: 'Profile',
+				action: 'profile'
+			},
+			{
+				title: 'Your posts',
+				action: 'profile/posts'
+			},
+			{
+				title: 'Messages',
+				action: 'profile/messages'
+			},
+			{
+				title: 'Settings',
+				action: 'settings'
+			}
+		];
+		
+		$scope.toggleLeft = function( ) {
+			$ionicSideMenuDelegate.toggleLeft();
+		};
+		
+		
+	});
+	
