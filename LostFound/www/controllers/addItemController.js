c88e61d9a1806d448f63489877da839cc9bbdeac
Calabo <cgomez150@gmail.com>
2015-11-16 03:07:23 -0500

    add addItemForm template and addItemController

---
+++ LostFound/www/controllers/addItemController.js
@@ -0,0 +1,26 @@
+angular.module( 'starter' )
+	.controller('addItemController', function($scope, $ionicModal) 
+	{
+	  $ionicModal.fromTemplateUrl('www/templates/form/addItemform.html', 
+	  {
+	    scope: $scope,
+	    animation: 'slide-in-up'
+	  }).then(function(modal) 
+	  {
+	    $scope.modal = modal;
+	  });
+	  $scope.openModal = function() 
+	  {
+	    $scope.modal.show();
+	  };
+	  $scope.closeModal = function() 
+	  {
+	    $scope.modal.hide();
+	  };
+	  //Cleanup the modal when we're done with it!
+	  $scope.$on('$destroy', function() 
+	  {
+	    $scope.modal.remove();
+	  });
+	  
+	});
