147767ce8663c0be6a6d837ecd794ad9bf4e52e9
ketushah <ketuindia@gmail.com>
2015-11-30 14:50:10 -0500

    Update itemController.js

--- LostFound/itemController.js
+++ LostFound/itemController.js
@@ -3,7 +3,7 @@ app.post('/addItem',funvtion(req,res){
 		"name"	  : req.body.name,    //I have used the body tag here but there was no body tag in the UI. 
 		"finderID"  : //not sure how to get this
 		"locationID": req.body.location,
-		"picture"   : req.body.imageURL,
+		"picture"   : req.body.imageURL, // we only need URL where the image will be stored in local machine 
 		"details"   : req.body.detail,
 		"date"      : req.body.date,
 		"closed"    : { type: Boolean, required: true }
